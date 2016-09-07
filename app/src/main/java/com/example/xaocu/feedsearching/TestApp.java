5c0e48db2509dfdd769563dda25e4159ae06d7b3
Iurii Kushyk <xaocuk1@gmail.com>
2016-09-07 14:00:49 +0300

    - Created Application for project - Added to manifest permission INTERNET, NETWORK_STATE

---
+++ app/src/main/java/com/example/xaocu/feedsearching/TestApp.java
@@ -0,0 +1,32 @@
+package com.example.xaocu.feedsearching;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.xaocu.feedsearching.net.NetManager;
+
+/**
+ * Created by Iurii Kushyk on 04.09.2016.
+ */
+public class TestApp extends Application {
+  private static NetManager manager;
+  private static Context applicationContext;
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    applicationContext = this;
+    initNetManager();
+  }
+
+  public static Context getContext() {
+    return applicationContext;
+  }
+
+  private void initNetManager() {
+    manager = new NetManager();
+  }
+
+  public static NetManager getManager() {
+    return manager;
+  }
+}
